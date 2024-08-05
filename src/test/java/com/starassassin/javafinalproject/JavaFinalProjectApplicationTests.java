622b84a61d08613f61e86d2aee46a04b8be7dc7c
StarAssassin64 <starassassin64@starassassin64.com>
2024-08-05 08:07:41 -0500

    Created Project

---
+++ src/test/java/com/starassassin/javafinalproject/JavaFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starassassin.javafinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFinalProjectApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
